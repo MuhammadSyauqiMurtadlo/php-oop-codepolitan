1cd05d26d9dd03504fd5e26bcc77e075324b67bc
Syauqi <sauki084@gmail.com>
2025-10-24 08:41:07 +0700

    Encapsulation class

---
+++ chapter-3/encapsulation-class.php
@@ -0,0 +1,52 @@
+<?php
+
+class User
+{
+  public $firstName;
+  public $lastName;
+  public $username;
+
+  protected $regID = 1001;
+  private $level = 'User';
+
+  function fullname()
+  {
+    return $this->firstName . " " . $this->lastName;
+  }
+
+  protected function sayProtect()
+  {
+    return "Hello, Protected";
+  }
+
+  private function sayPrivate()
+  {
+    return "Hello, Private";
+  }
+}
+
+class Customer extends User
+{
+  public function sayParent()
+  {
+    return $this->sayPrivate();
+  }
+}
+
+$u = new User;
+$u->firstName = "User";
+$u->lastName = "Class";
+
+// echo $u->$regID . "<br/>";
+// echo $u->$level . "<br/>";
+
+echo $u->fullname() . "<br/>";
+// echo $u->sayProtect() . "<br/>";
+// echo $u->sayPrivate() . "<br/>";
+echo $u->sayPrivate() . "<br/>";
+
+// $customer1 = new Customer();
+// $customer1->firstName = "John";
+// $customer1->lastName = "Doe";
+
+// echo $customer1->fullname(); // Outputs: John Doe
